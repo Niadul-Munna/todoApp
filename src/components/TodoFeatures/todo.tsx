c7ad526baa1c0fdd8c883487f7dd43421615f5da
niadulMunna <niadulislammunna@gmail.com>
2025-01-19 23:15:51 +0600

    refactor

--- src/components/TodoFeatures/todo.tsx
+++ src/components/TodoFeatures/todo.tsx
@@ -7,11 +7,6 @@ import TodoList from "./todoList";
 const Todo = () => {
   const [display, setDisplay] = useState("Walk");
 
-  const add = (e) => {
-    setDisplay(e.preventDefault.value);
-    console.log(display);
-  };
-
   return (
     <>
       {/* --------Title------- */}
@@ -23,15 +18,11 @@ const Todo = () => {
       {/* --------input box------- */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
-          onChange={setDisplay}
           type="text"
           placeholder="Add Your Task"
           className="bg-transparent border-0 outline-none h-14 pl-6 pr-2 flex-1 placeholder:text-slate-600"
         />
-        <Button
-          onClick={add}
-          className="w-32 h-14 rounded-full text-lg font-medium bg-orange-600 hover:bg-orange-500 hover:text-xl"
-        >
+        <Button className="w-32 h-14 rounded-full text-lg font-medium bg-orange-600 hover:bg-orange-500 hover:text-xl">
           Add +
         </Button>
       </div>
