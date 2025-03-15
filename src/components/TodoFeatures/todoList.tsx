e98ecf5f54abc116b885f9a509cfa0d8e99ef65d
niadulMunna <niadulislammunna@gmail.com>
2025-03-15 22:07:22 +0600

    style: hover duration

--- src/components/TodoFeatures/todoList.tsx
+++ src/components/TodoFeatures/todoList.tsx
@@ -7,7 +7,7 @@ interface TodoListProps {
 
 const TodoList = ({ task }: TodoListProps) => {
   return (
-    <div className="border border-gray-300 rounded-md p-3 my-4 hover:bg-gray-300">
+    <div className="border border-gray-300 rounded-md p-3 my-4 duration-300 hover:bg-gray-300">
       <div className="flex items-center gap-2 my-4 text-lg">
         <div className="flex flex-1 items-center gap-2">
           <LucideCheckCircle />
